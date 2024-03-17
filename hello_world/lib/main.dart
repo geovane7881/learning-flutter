62cac800330f2ac3414d224abcc7c81008228eb1
Geovane Clemente <geo.zn13@gmail.com>
2024-03-17 13:25:47 -0300

    hello world

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Container(
+      color: Colors.black,
+      alignment: Alignment.center,
+      child: Text('Olá mundo!',
+          textAlign: TextAlign.center
+      ),
+    ),
+  ));
+}
